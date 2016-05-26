e2af3cf417be118089d7964820d5471c4b334740
Si Ku <Cyber-SiKu@outlook.com>
2016-05-26 20:32:18 +0800

    some changes
    some changes still wrong with something

--- L2-001. 紧急救援.cpp
+++ L2-001. 紧急救援.cpp
@@ -25,7 +25,7 @@ int main() {
 		std::cin>>s>>d>>length;
 		map_city[d][s] = map_city[s][d] = length;
 	}
-	//store the path of the shorest path,without start and end point
+	//store the shorest path of the S to i,without end point
 	std::queue<int> path[MAX_N] ;
 
 	count_least_path(path,map_city,city,S,D,N);
@@ -37,23 +37,24 @@ void count_least_path(std::queue<int> path[],int map_city[][MAX_N],int city[],
 	int i = 0;  //record the point hase visted
 
 	int min = MAX_N +1; //tips:the length is less than 500
-	int min_j = MAX_N;
+	int min_j = MAX_N + 1;
 	int min_k = S;
 	visted[S] = true;
 
-	while(1) {  //if you want to find one path to change 1 to path[i] != N
+	while(1) {  
+		//if you want to find one path to change 1 to path[i] != N
 		i++;
 		//find the nearest point to visted
-		for(int j = 0; j < N; j++) {    //visted point
-			if(!visted[j]) {
-				continue;
-			}
-			for(int k = 0; k < N; k ++) { //unvisted ponit
-				if(!visted[k] && map_city[j][k]) {
-					
-				min_j = min > map_city[j][k] ? j : min_j;
-				min_k = min > map_city[j][k] ? k : min_k;
-				min = min > map_city[j][k] ? map_city[j][k] : min;
+		for(int j = 0; j < N; j++) {    
+			//visted point
+			if(visted[j]) {
+				for(int k = 0; k < N; k ++) { 
+					//unvisted ponit and connect with point j
+					if(!visted[k] && map_city[j][k]) {
+						min_j = min > map_city[j][k] ? j : min_j;
+						min_k = min > map_city[j][k] ? k : min_k;
+						min = min > map_city[j][k] ? map_city[j][k] : min;
+					}
 				}
 			}
 		}
@@ -68,19 +69,18 @@ void count_least_path(std::queue<int> path[],int map_city[][MAX_N],int city[],
 	}
 
 	int min_length = map_city[S][min_j] + map_city[min_j][D];
-	int max_number_team = city[min_j];
+	int max_number_team = city[min_j] + city[D];
 	int count = 0;
 
 	for(int i = 0; i < N; i++) {
-		if(!visted[i] || !map_city[i][D]) {
-			//point i->D not connect or point i is not vistedd continue
-			continue;
-		}
-		if(min_length == map_city[i][D]) {
-			count++;
-			min_j = max_number_team >= city[i]+city[D] ? min_j : i;
-			max_number_team = max_number_team >= city[i]+city[D] ? max_number_team : city[i]
-			                  +city[D];
+		if(visted[i] && map_city[i][D]) {
+			if(min_length == map_city[i][D]) {
+				//find one match the require
+				count++;
+				min_j = max_number_team >= city[i]+city[D] ? min_j : i;
+				max_number_team = max_number_team >= city[i]+city[D] ? max_number_team : city[i]
+								  +city[D];
+			}
 		}
 	}
 
@@ -104,9 +104,8 @@ void update_length(int map_city[][MAX_N],bool visted[],std::queue<int> path[],
 	map_city[S][k] = map_city[k][S] = map_city[S][j] + map_city[j][k];
 
 	for(int i = 0; i < N; i++) {
-		if(visted[i] || !map_city[k][i]) {
-			continue;
+		if(!visted[i] && map_city[k][i]) {
+			map_city[i][k] = map_city[k][i] = map_city[i][k] + map_city[S][k];
 		}
-		map_city[i][k] = map_city[k][i] = map_city[i][k] + map_city[S][k];
 	}
 }
