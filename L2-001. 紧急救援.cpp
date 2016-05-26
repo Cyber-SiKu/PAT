0d918dd7c70c52f09e0e9af604f5a3cf34c739b4
Si Ku <Cyber-SiKu@outlook.com>
2016-05-26 18:10:01 +0800

    half-production
    still have some problem to slove
    
    the test as follow
    
    测试点 结果 用时(ms) 内存(kB) 得分/满分
    0 答案正确 2 1536 12/12
    1 答案错误 2 1576 0/7
    2 答案正确 2 1592 2/2
    3 答案错误 127 1792 0/4

--- L2-001. 紧急救援.cpp
+++ L2-001. 紧急救援.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
-#include<stack>
+#include<queue>
 
 const int MAX_N = 500;
 
-int count_least_path(std::stack<int> path[],int map_city[][MAX_N],int city[],
-                     const int & S,const int & D);
-void update_length(int map_city[][MAX_N],bool visted[],std::stack<int> path[]
-                   ,int city[],const int & S,const int & j,const int & k);   //after visted point k
+void count_least_path(std::queue<int> path[],int map_city[][MAX_N],int city[],
+                      const int & S,const int & D,const int & N);
+void update_length(int map_city[][MAX_N],bool visted[],std::queue<int> path[]
+                   ,int city[],const int & S,const int & j,const int & k,
+                   const int & N);   //after visted point k
 //j-k is the short length
 
 int main() {
@@ -25,32 +26,36 @@ int main() {
 		map_city[d][s] = map_city[s][d] = length;
 	}
 	//store the path of the shorest path,without start and end point
-	std::stack<int> path[MAX_N] ;
+	std::queue<int> path[MAX_N] ;
 
-	count_least_path(path,map_city,city,S,D);
+	count_least_path(path,map_city,city,S,D,N);
 	return 0;
 }
-int count_least_path(std::stack<int> path[],int map_city[][MAX_N],int city[],
-                     const int & S,const int & D) {
+void count_least_path(std::queue<int> path[],int map_city[][MAX_N],int city[],
+                      const int & S,const int & D,const int & N) {
 	bool visted[MAX_N] = {false};   //the point is visted or not
 	int i = 0;  //record the point hase visted
 
 	int min = MAX_N +1; //tips:the length is less than 500
 	int min_j = MAX_N;
 	int min_k = S;
+	visted[S] = true;
 
 	while(1) {  //if you want to find one path to change 1 to path[i] != N
 		i++;
 		//find the nearest point to visted
-		for(int j = 0; j < i; j++) {    //visted point
-			for(int k = 0; k < MAX_N; k ++) { //unvisted ponit
-				if(visted[k]) {
+		for(int j = 0; j < N; j++) {    //visted point
+			if(!visted[j]) {
+				continue;
+			}
+			for(int k = 0; k < N; k ++) { //unvisted ponit
+				if(visted[k] || !map_city[j][k]) {
 					continue;
 				}
 				//TODO: when finished change follow to if ,to see which is quick;
-				min = min > map_city[j][k] ? map_city[j][k] : min;
 				min_j = min > map_city[j][k] ? j : min_j;
 				min_k = min > map_city[j][k] ? k : min_k;
+				min = min > map_city[j][k] ? map_city[j][k] : min;
 			}
 		}
 
@@ -58,48 +63,51 @@ int count_least_path(std::stack<int> path[],int map_city[][MAX_N],int city[],
 		if(min_k == D) {
 			break;
 		}
-		update_length(map_city,visted,path,city,S,min_j,min_k);
+
+		update_length(map_city,visted,path,city,S,min_j,min_k,N);
 		min = MAX_N +1;
 	}
 
-	int min_length = map_city[S][min_j] + map_city[min_j][min_k];
+	int min_length = map_city[S][min_j] + map_city[min_j][D];
 	int max_number_team = city[min_j];
 	int count = 0;
-	for(int i = 0; i < MAX_N; i++) {
-		if(!visted[i]) {
+
+	for(int i = 0; i < N; i++) {
+		if(!visted[i] || !map_city[i][D]) {
+			//point i->D not connect or point i is not vistedd continue
 			continue;
 		}
-		if(min_length == map_city[S][i] + map_city[i][D]) {
+		if(min_length == map_city[i][D]) {
 			count++;
-			max_number_team = max_number_team > city[i] ? max_number_team : city[i];
+			min_j = max_number_team >= city[i]+city[D] ? min_j : i;
+			max_number_team = max_number_team >= city[i]+city[D] ? max_number_team : city[i]
+			                  +city[D];
 		}
 	}
 
-	std::cout<<count<<" "<<max_number_team<<std::endl
-	         <<S;
+	std::cout<<count<<" "<<max_number_team<<std::endl;
 	while(!path[min_j].empty()) {
-		int i;
-		//TODO : ²é¿´stack¶¥ÔªËØ
+		std::cout<<path[min_j].front()<<" ";
 		path[min_j].pop();
-		std::cout<<" "<<i;
 	}
-	std::cout<<" "<<min_j<<" "<<D;
+	std::cout<<min_j<<" "<<D<<std::endl;
 }
-void update_length(int map_city[][MAX_N],bool visted[],std::stack<int> path[],
-                   int city[],const int & S,const int & j,const int & k) {
+void update_length(int map_city[][MAX_N],bool visted[],std::queue<int> path[],
+                   int city[],const int & S,const int & j,const int & k,const int & N) {
 	visted[k] = true;
 
-	//store S->k's shortest length without start and end ponit
+	//store S->k's shortest length without end ponit
 	path[k] = path[j];
 	path[k].push(j);
 
 	//store S->k's rescue team's number
 	city[k] += city[j];
+	map_city[S][k] = map_city[k][S] = map_city[S][j] + map_city[j][k];
 
-	for(int i = 0; i < MAX_N; i++) {
+	for(int i = 0; i < N; i++) {
 		if(visted[i] || !map_city[k][i]) {
 			continue;
 		}
-		map_city[S][k] = map_city[k][S] = map_city[S][j] + map_city[k][i];
+		map_city[i][k] = map_city[k][i] = map_city[i][k] + map_city[S][k];
 	}
 }
