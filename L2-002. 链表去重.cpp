1402a739edf5fdc6de9bdc2d00e54474c49b6b81
Si Ku <Cyber-SiKu@outlook.com>
2016-05-28 16:23:51 +0800

    still have some
    测试点
    
    测试点 结果 用时(ms) 内存(kB) 得分/满分
    0 答案正确 1 256 13/13
    1 答案错误 1 304 0/3
    2 答案正确 1 268 2/2
    3 运行超时   0/4
    4 运行超时   0/3

--- L2-002. 链表去重.cpp
+++ L2-002. 链表去重.cpp
@@ -1,6 +1,64 @@
 #include<iostream>
+#include<map>
+#include<utility>
+#include<iterator>
+#include<string>
 
-int main()
-{
+void church(std::map<std::string, std::pair<const int, std::string>> & L1,
+            const std::string & start1,
+            std::string & start2);
+
+int main() {
+	std::map<std::string, std::pair<const int, std::string>> L1;
+	//key : Adress;value :{Key,Next}
+	std::string start1;
+	int N;
+	std::cin >> start1 >> N;
+	for (int i = 0; i < N; i++) {
+		std::string Adress, Next;
+		int Key;
+		std::cin >> Adress >> Key >> Next;
+
+		L1.insert(std::pair<std::string, std::pair<const int, std::string>>(Adress,
+		          std::pair<const int, std::string>(Key, Next)));
+	}
+
+	std::string start2 = "-1";
+	church(L1, start1, start2);
+	while (start1 != "-1") {
+		std::cout << start1 << " " << L1[start1].first << " " << L1[start1].second <<
+		          std::endl;
+		start1 = L1[start1].second;
+	}
+	while (start2 != "-1") {
+		std::cout << start2 << " " << L1[start2].first << " " << L1[start2].second <<
+		          std::endl;
+		start2 = L1[start2].second;
+	}
 	return 0;
+}
+
+void church(std::map<std::string, std::pair<const int, std::string>> & L1,
+            const std::string & start1,
+            std::string & start2) {
+	std::string flag = "-1";
+	for (std::string i = start1; i != "-1"; i = L1[i].second) {
+		for (std::string j = i; L1[j].second != "-1"; j = L1[j].second) {
+			if (L1[i].first == L1[L1[j].second].first
+			        || L1[i].first == - L1[L1[j].second].first) {
+				std::string temp = L1[j].second;
+				L1[j].second = L1[temp].second;
+				L1[temp].second = "-1";
+				if(flag == "-1") {
+					start2 = temp;
+					flag = temp;
+				} else {
+					L1[flag].second = temp;
+				}
+				if(L1[j].second == "-1") {
+					break;
+				}
+			}
+		}
+	}
 }
