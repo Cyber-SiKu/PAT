4dcaa5254d5b8253bd60b82d111e10ae7feb2ce5
Si Ku <Cyber-SiKu@outlook.com>
2016-05-30 08:21:38 +0800

    a o,some problem
    测试点
    
    测试点 结果 用时(ms) 内存(kB) 得分/满分
    0 答案正确 1 256 12/12
    1 答案正确 1 432 3/3
    2 答案正确 1 384 3/3
    3 答案正确 17 312 3/3
    4 运行超时   0/4查看代码

---
+++ L2-005. 集合相似度.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<set>
+#include<vector>
+#include<cstdio>
+#include<iterator>
+
+float Similar_rate(std::vector<std::set<int>> & v,const int & number1,
+                   const int & number2);
+
+int main() {
+	int N;
+	std::cin>>N;
+	std::vector<std::set<int>> v;
+	for(int i = 0; i < N; i++) {
+		int M;
+		std::cin>>M;
+
+		std::set<int> set_temp;
+		for(int j = 0; j < M; j++) {
+			int temp;
+			std::cin>>temp;
+			set_temp.insert(temp);
+		}
+		v.push_back(set_temp);
+	}
+	int K;
+	std::cin>>K;
+	for(int i = 0; i < K; i++) {
+		int number1,number2;
+		std::cin>>number1>>number2;
+		printf("%0.2f%\n",100*Similar_rate(v,number1,number2));
+	}
+	return 0;
+}
+
+float Similar_rate(std::vector<std::set<int>> & v,const int & number1,
+                   const int & number2) {
+	float Nc = 0;
+	for(std::set<int>::iterator i = v[number2-1].begin(); i != v[number2-1].end();
+	        i++) {
+		for(std::set<int>::iterator j = v[number1-1].begin(); j != v[number1-1].end();
+		        j++) {
+			if(*i == *j) {
+				Nc+=1;
+			}
+		}
+	}
+	std::set<int> temp(v[number1-1]);
+	temp.insert(v[number2-1].begin(),v[number2-1].end());
+
+	float Nt = temp.size();
+	return Nc / Nt;
+}
